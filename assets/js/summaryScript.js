d830ef8ebd5dd6d707bcb047fb1d7b85bfc6c72b
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2023-05-30 07:40:03 +0330

    added script file to show summary list

--- assets/js/summaryScript.js
+++ assets/js/summaryScript.js
@@ -1,1 +1,30 @@
 
+fetch('data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let jsonData = data;
+    let listItems = document.getElementById("summaryList");
+    let counter = 1;
+
+    jsonData.forEach(function (item) {
+      let summaryList = document.createElement('li');
+      let classItem = "row-" + counter;
+      summaryList.classList.add(classItem)
+      summaryList.innerHTML =
+        '<h4>' +
+        item.category +
+        '</h4>' +
+        '<span>' +
+        item.score +
+        '</p>' +
+        '<img src="' +
+        item.icon +
+        '" alt="' +
+        item.category +
+        ' icon">';
+        listItems.appendChild(summaryList);
+        counter++;
+    });
+  });

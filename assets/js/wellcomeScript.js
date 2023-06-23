8eda949c9aa4199930529acf647c8864494ef8f6
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2023-06-24 01:32:50 +0330

    ready for publish

---
+++ assets/js/wellcomeScript.js
@@ -0,0 +1,12 @@
+const showBack = document.getElementById("product")
+const showSummary = document.getElementById("summaryCard")
+
+setTimeout(() => {
+    showSummary.classList.add("showSummary")
+}, 2000);
+
+setTimeout(() => {
+    showBack.classList.add("showBack")
+}, 2000);
+
+
